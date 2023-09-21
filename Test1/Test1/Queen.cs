d20b19fd9934968cfcf5c7010655a0715d4073bc
GhostMalthael <145560047+GhostMalthael@users.noreply.github.com>
2023-09-21 15:45:48 +0500

    Update Queen.cs

--- Test1/Test1/Queen.cs
+++ Test1/Test1/Queen.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,50 +8,47 @@ namespace Test1
 {
     internal class Queen: Figures
     {
-        Figures figures;
-        Chess chess;
-        int newQueenX;
-        int newQueenY;
+        
+        private new int xPos;
+        private new int yPos;
         public virtual int NewQueenX 
         { 
-            get { return newQueenX; }
-            set { newQueenX = value; }
+            get { return xPos; }
         }
         public virtual int NewQueenY 
         {
-            get { return newQueenY; }
-            set { newQueenY = value; }
+            get { return yPos; }
         }
         
-        public Queen(string name, string symbol, int xPos, int yPos, Chess chess ) : base(name, symbol, xPos, yPos)
+        public Queen(string name, string symbol, int xPos, int yPos) : base(name, symbol)
         {
-            
-            this.chess = chess;
+            this.xPos = 7;
+            this.yPos = yPos;
         }
 
-        public void MoveQueen()
+        public override void Hod()
         {
             Console.WriteLine("Куда переместить ферзя?");
-            bool secondX = int.TryParse(Console.ReadLine(), out newQueenX);
-            bool secondY = int.TryParse(Console.ReadLine(), out newQueenY);
-            if(secondX && secondY == false) 
+            bool checkX = int.TryParse(Console.ReadLine(), out xPos);
+            bool checkY = int.TryParse(Console.ReadLine(), out yPos);
+            if(checkX && checkY == false) 
             {
                 Console.WriteLine("Вы не выбрали поля или указан неверный формат, повторите попытку:");
             }
-            else if (newQueenX < 0 || newQueenX > 7 || newQueenY < 0 || newQueenY > 7)
+            else if (NewQueenX < 0 || NewQueenX > 7 || NewQueenY < 0 || NewQueenY > 7)
             {
                 Console.WriteLine("Конец поля, повторите попытку");
             }
-            else if(Math.Abs(xPos - newQueenX) == Math.Abs(yPos - newQueenY) ||
-                Math.Abs(xPos + newQueenX) == Math.Abs(yPos + newQueenY) ||
-                this.xPos == newQueenX && newQueenY > this.yPos ||
-                this.xPos == newQueenX && newQueenY < this.yPos ||
-                this.yPos == newQueenY && newQueenX > this.xPos ||
-                this.yPos == newQueenY && newQueenX < this.xPos
+            else if(Math.Abs(xPos - NewQueenX) == Math.Abs(yPos - NewQueenY) ||
+                Math.Abs(xPos + NewQueenX) == Math.Abs(yPos + NewQueenY) ||
+                this.xPos == NewQueenX && NewQueenY > this.yPos ||
+                this.xPos == NewQueenX && NewQueenY < this.yPos ||
+                this.yPos == NewQueenY && NewQueenX > this.xPos ||
+                this.yPos == NewQueenY && NewQueenX < this.xPos
                 )
             {
-                this.xPos = newQueenX;
-                this.yPos = newQueenY;
+                this.xPos = NewQueenX;
+                this.yPos = NewQueenY;
             }
             else
             {
